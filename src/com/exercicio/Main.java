d020b3ceaad07aa25572219380403e9662c507a9
day <daynaramira@gmail.com>
2022-07-19 20:05:03 -0300

    Exercício02

---
+++ src/com/exercicio/Main.java
@@ -0,0 +1,11 @@
+package com.exercicio;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercício do Quadrilátero");
+        Quadrilatero.area(5);
+        Quadrilatero.area(3d,3d);
+        Quadrilatero.area(8,5,9);
+        Quadrilatero.area(6f,6f);
+    }
+}
